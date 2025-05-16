2b01f9a2fb3684a25826b976b5e21735189fca2d
Cristi Calin <cristicalin102@gmail.com>
2025-05-16 15:10:32 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DeltaSocial.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
